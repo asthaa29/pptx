81ee02ac5b25bcdb50229a8bc03524ac1cf30093
asthaa29 <63576065+asthaa29@users.noreply.github.com>
2021-02-12 00:06:30 +0530

    Fixed image insert issue

--- pptx/shapes/placeholder.py
+++ pptx/shapes/placeholder.py
@@ -313,7 +313,7 @@ class PicturePlaceholder(_BaseSlidePlaceholder):
     Placeholder shape that can only accept a picture.
     """
 
-    def insert_picture(self, image_file):
+    def insert_picture(self, image_file, method = 'crop'):
         """
         Return a |PlaceholderPicture| object depicting the image in
         *image_file*, which may be either a path (string) or a file-like
@@ -323,11 +323,11 @@ class PicturePlaceholder(_BaseSlidePlaceholder):
         :attr:`~._BaseSlidePlaceholder.shape_type` property is
         `MSO_SHAPE_TYPE.PLACEHOLDER` instead of `MSO_SHAPE_TYPE.PICTURE`.
         """
-        pic = self._new_placeholder_pic(image_file)
+        pic = self._new_placeholder_pic(image_file, method)
         self._replace_placeholder_with(pic)
         return PlaceholderPicture(pic, self._parent)
 
-    def _new_placeholder_pic(self, image_file):
+    def _new_placeholder_pic(self, image_file, method = 'crop'):
         """
         Return a new `p:pic` element depicting the image in *image_file*,
         suitable for use as a placeholder. In particular this means not
@@ -336,8 +336,28 @@ class PicturePlaceholder(_BaseSlidePlaceholder):
         """
         rId, desc, image_size = self._get_or_add_image(image_file)
         shape_id, name = self.shape_id, self.name
-        pic = CT_Picture.new_ph_pic(shape_id, name, desc, rId)
-        pic.crop_to_fit(image_size, (self.width, self.height))
+
+        # Cropping the image, as in the original file
+        if method == 'crop':
+            pic = CT_Picture.new_ph_pic(shape_id, name, desc, rId)
+            pic.crop_to_fit(image_size, (self.width, self.height))
+
+        # Adjusting image to placeholder size and replace placeholder.     
+        else:
+            aspectImg = image_size[0]/image_size[1]
+            aspectPh  = self.width / self.height
+
+            if aspectPh > aspectImg:
+                h = self.height
+                w = int(aspectImg * self.height)
+
+            else:
+                w = self.width
+                h = int(aspectImg * w)
+
+            pic = CT_Picture.new_pic(shape_id, name, desc, rId, self.left, self.top, w, h)
+
+
         return pic
 
     def _get_or_add_image(self, image_file):
